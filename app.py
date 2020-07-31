ceb6cc307f9fe327f53c57c18cf7d4e648a147c6
Justin Shymko <jashymko@gmail.com>
2020-07-30 18:16:50 -0600

    Restructured 2P reversi

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 """
-- Skip turn if no moves can be made
-    - End game if neither player can make a move
-- Fix your hardcoding
+- Minimax
+- Make it look pretty
 """
-
 import pygame
+from reversi import Reversi
 
-WIDTH = 540
-HEIGHT = 540
-P_COLORS = (None, (0, 0, 0), (255, 255, 255))
+#          BG Color,    P1 Color,    P2 Color,    Outline Color
+COLORS = ((0, 128, 0), (0, 0, 0), (255, 255, 255), (0, 0, 0))
+WIDTH, HEIGHT = (540, 540)
 
-grid = [[0] * 8 for _ in range(8)]
-turn = 1    #1 & 2
+################################################################
+
+screen = None
+game = Reversi()
+
 mouse_held = False
-overlapping_moves = []
 
 
 def init_game():
-    global screen, possible_moves
+    global screen
 
     pygame.init()
     pygame.display.set_caption("Reversi")
     pygame.display.set_icon(pygame.image.load("icon.png"))
+
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-    grid[3][3], grid[3][4], grid[4][3], grid[4][4] = 2, 1, 1, 2
-
-    possible_moves = find_moves()
 
     # Game Loop
     running = True
@@ -38,102 +36,46 @@ def init_game():
         draw_all()
 
 
-def opp():
-    return 3 - turn
-
-
-def scan(opp_row, opp_column, direction):
-    row_change, column_change = direction[0], direction[1]
-    row, column = opp_row + row_change, opp_column + column_change
-
-    opp_found = False
-    while 0 <= row <= 7 and 0 <= column <= 7:   # Search in given direction for valid move
-        if grid[row][column] == turn:
-            break
-        elif grid[row][column] == 0 and not opp_found:
-            break
-        elif grid[row][column] == 0 and opp_found:
-            return ((column, row), direction)
-        elif grid[row][column] == opp():
-            opp_found = True
-        row += row_change
-        column += column_change
-    return
-
-def find_moves():
-    possible_moves = []
-    for row in range(8):
-        for column in range(8):
-            if grid[row][column] == turn:
-                dirs = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))   # Check every direction on each of player's pieces for potential connection
-                for direction in dirs:
-                    move = scan(row, column, direction)
-                    if move:
-                        possible_moves.append(move)
-    return possible_moves
-
-
-def change_turn():
-    global overlapping_moves, turn, possible_moves
-    overlapping_moves = []
-    turn = opp()
-    possible_moves = find_moves()
-
-
 def handle_events():
-    import time
-    global grid, mouse_held, possible_moves, overlapping_moves, turn
+    global mouse_held
 
-    # Mouse Events, turn action on mouseup
+    # Mouse Events, make move on mouseup
     pressed = False
     for button in pygame.mouse.get_pressed():
         if button:
             pressed = True
+
     if pressed and not mouse_held:
         mouse_held = True
+
     elif mouse_held and not pressed:    # Mouseup
         mouse_held = False
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        mouse_x = 8*mouse_x // WIDTH
-        mouse_y = 8*mouse_y // HEIGHT
-        possible_moves = find_moves()
-        for move in possible_moves:
-            if move[0] == (mouse_x, mouse_y):
-                overlapping_moves.append(move)  # Find move(s) in possible_moves,
-        for move in overlapping_moves:          # fill in cells with player's chips
-            row_change, column_change = move[1][0], move[1][1]
-            column, row = mouse_x, mouse_y
-            while grid[row][column] != turn:
-                grid[row][column] = turn
-                row -= row_change
-                column -= column_change
-            grid[mouse_y][mouse_x] = 0
-        if overlapping_moves:
-            grid[mouse_y][mouse_x] = turn
-            change_turn()
-        elif not possible_moves:    # Skip turn on click if no moves are possible
-            change_turn()
+        mouse_x = game.GRID_W*mouse_x // WIDTH
+        mouse_y = game.GRID_W*mouse_y // HEIGHT
+        game.move(mouse_x, mouse_y)
 
 
 def draw_all():
     # Draw Board
-    pygame.draw.rect(screen, (0, 128, 0), (0, 0, WIDTH, HEIGHT))
+    pygame.draw.rect(screen, COLORS[0], (0, 0, WIDTH, HEIGHT))
     for i in range(9):
-        pygame.draw.line(screen, (0, 0, 0), (i * WIDTH/8, 0), (i * WIDTH/8, HEIGHT))
-        pygame.draw.line(screen, (0, 0, 0), (0, i * HEIGHT/8), (WIDTH, i * HEIGHT/8))
+        pygame.draw.line(screen, COLORS[3], (i * WIDTH/game.GRID_W, 0), (i * WIDTH/game.GRID_W, HEIGHT))
+        pygame.draw.line(screen, COLORS[3], (0, i * HEIGHT/game.GRID_W), (WIDTH, i * HEIGHT/game.GRID_W))
 
     # Draw Disks
-    for row in range(8):
-        for column in range(8):
-            cell = grid[row][column]
+    for row in range(game.GRID_W):
+        for column in range(game.GRID_W):
+            cell = game.grid[row][column]
             if cell:
-                pygame.draw.circle(screen, P_COLORS[cell], (int((column + 0.5) * WIDTH/8), int((row + 0.5) * HEIGHT/8)), min(WIDTH, HEIGHT)/16 - 2)
+                pygame.draw.circle(screen, COLORS[cell], (int((column + 0.5) * WIDTH/game.GRID_W), int((row + 0.5) * HEIGHT/game.GRID_W)), min(WIDTH, HEIGHT)/(2*game.GRID_W) - 2)
 
-    #Draw Move Markers
-    for move in possible_moves:
+    # Draw Markers
+    for move in game.find_moves():
         column, row = move[0]
-        pygame.draw.circle(screen, P_COLORS[turn], (int((column + 0.5) * WIDTH/8), int((row + 0.5) * HEIGHT/8)), min(WIDTH, HEIGHT)/128)
+        pygame.draw.circle(screen, COLORS[game.turn], (int((column + 0.5) * WIDTH/game.GRID_W), int((row + 0.5) * HEIGHT/game.GRID_W)), min(WIDTH, HEIGHT) / (16*game.GRID_W))
 
     pygame.display.update()
+
 
 init_game()
